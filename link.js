4f2726d872dc7afbb3950c1f7806c97a94389141
Ajito <107695837+ajito89@users.noreply.github.com>
2022-08-16 16:34:09 +0900

    Update link.js

--- link.js
+++ link.js
@@ -3,3 +3,9 @@ https://ajito89.github.io/mywstore/abcd.png
 https://ajito89.github.io/mywstore/abcde.png
 https://ajito89.github.io/mywstore/abcdef.png
 https://ajito89.github.io/mywstore/abcdefg.png
+
+https://ajito89.github.io/mywstore/ajito@email.com.png
+https://ajito89.github.io/mywstore/nelson@email.com.png
+https://ajito89.github.io/mywstore/lucio@email.com.png
+https://ajito89.github.io/mywstore/da@email.com.png
+https://ajito89.github.io/mywstore/costa@email.com.png
