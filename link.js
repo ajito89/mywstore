06b29de9e350b61ba92fa550b78a38ce5189537c
Ajito <107695837+ajito89@users.noreply.github.com>
2022-08-16 13:52:23 +0900

    Create link.js
    link for pic

---
+++ link.js
@@ -0,0 +1,5 @@
+https://ajito89.github.io/mywstore/abc.png
+https://ajito89.github.io/mywstore/abcd.png
+https://ajito89.github.io/mywstore/abcde.png
+https://ajito89.github.io/mywstore/abcdef.png
+https://ajito89.github.io/mywstore/abcdefg.png
